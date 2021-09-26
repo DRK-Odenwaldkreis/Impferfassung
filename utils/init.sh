cd76cd00a1bafa58b904a964898025e6feaf9a3f
mbay <murat.bayram@softwareag.com>
2021-09-26 16:24:58 +0200

    changing init files

--- utils/init.sh
+++ utils/init.sh
@@ -2,5 +2,5 @@
 
 # This file is part of DRK Impfterminerfassung.
 
-cp /home/webservice/Backup/logo.png /home/webservice/Impfterminerfassung/utils/logo.png
-cp /home/webservice/Backup/config.ini /home/webservice/Impfterminerfassung/config.ini
+cp /home/webservice/Backup/logo_impfzentrum.png /home/webservice/Impfterminerfassung/utils/logo.png
+cp /home/webservice/Backup/config_impfzentrum.ini /home/webservice/Impfterminerfassung/config.ini
