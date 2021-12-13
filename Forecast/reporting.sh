274b0373675b2c79e3369dbae861f1434f2bb3dd
mbay <murat.bayram@softwareag.com>
2021-12-13 08:44:06 +0100

    error in bash script of wrong direction

--- Forecast/reporting.sh
+++ Forecast/reporting.sh
@@ -3,6 +3,6 @@
 # This file is part of DRK Testerfassung.
 
 echo "Starting Report"
-cd /home/webservice/Impfterminerfassung/TagesReport
+cd /home/webservice/Impfterminerfassung/Forecast
 python3 job.py $(date '+%Y-%m-%d') 1
 echo "Reporting complete"
